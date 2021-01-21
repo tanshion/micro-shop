97d13808597139e52de0eec44ad8b40a86d481e8
tanshion <405781027@qq.com>
2021-01-21 15:54:07 +0800

    feat: 新增mapper

--- micro-shop-service/src/main/java/com/abc1236/ms/query/BannerQuery.java
+++ micro-shop-service/src/main/java/com/abc1236/ms/query/BannerQuery.java
@@ -1,5 +1,6 @@
 package com.abc1236.ms.query;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -8,17 +9,36 @@ import java.util.Date;
 @Data
 public class BannerQuery {
     private Long id;
-    private Date createTime;
+
+    @ApiModelProperty(value = "创建人")
     private Long createBy;
-    private Date modifyTime;
+
+    @ApiModelProperty(value = "创建时间/注册时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "最后更新人")
     private Long modifyBy;
 
-    private String url;
+    @ApiModelProperty(value = "最后更新时间")
+    private Date modifyTime;
+
+    @ApiModelProperty(value = "banner图id")
+    private String idFile;
+
+    @ApiModelProperty(value = "界面")
+    private String page;
+
+    @ApiModelProperty(value = "参数")
+    private String param;
+
+    @ApiModelProperty(value = "标题")
     @NotBlank(message = "标题不能为空")
     private String title;
+
+    @ApiModelProperty(value = "类型")
     @NotBlank(message = "类型不能为空")
     private String type;
-    private String idFile;
-    private String page;
-    private String param;
+
+    @ApiModelProperty(value = "点击banner跳转到url")
+    private String url;
 }
