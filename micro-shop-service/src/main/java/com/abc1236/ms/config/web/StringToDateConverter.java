9ce4407b1482ee712668f3541af22d43713a0b23
tanshion <405781027@qq.com>
2021-04-16 16:39:31 +0800

    feat: mvc配置

---
+++ micro-shop-service/src/main/java/com/abc1236/ms/config/web/StringToDateConverter.java
@@ -0,0 +1,22 @@
+package com.abc1236.ms.config.web;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
+import org.springframework.core.convert.converter.Converter;
+import java.util.Date;
+
+public class StringToDateConverter implements Converter<String, Date> {
+
+
+    @Override
+    public Date convert(String source) {
+        if (StrUtil.isBlank(source)) {
+            return null;
+        }
+        return DateUtil.parse(source,
+            "yyyy-MM-dd HH:mm:ss","yyyy-MM-dd HH:mm","yyyy-MM-dd",
+            "yyyy/MM/dd HH:mm:ss","yyyy/MM/dd HH:mm","yyyy/MM/dd"
+        );
+    }
+
+}
