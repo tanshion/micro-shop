793252e81e1793c9c6ec3716a1987436feb5d38d
tanshion <405781027@qq.com>
2021-12-06 22:18:12 +0800

    refactor: dept

--- micro-shop-service/src/main/java/com/abc1236/ms/entity/system/Dept.java
+++ micro-shop-service/src/main/java/com/abc1236/ms/entity/system/Dept.java
@@ -4,9 +4,8 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-
 import java.util.Date;
+import lombok.Data;
 
 @Data
 @TableName(value = "t_sys_dept")
