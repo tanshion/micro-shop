1217f9408c272997ccbfecfe95c317faec5e5ee5
tanshion <843565561@qq.com>
2023-02-04 13:00:21 +0800

    feat: 优化

--- micro-shop-service/src/main/java/com/abc1236/ms/enumeration/cms/BannerTypeEnum.java
+++ micro-shop-service/src/main/java/com/abc1236/ms/enumeration/cms/BannerTypeEnum.java
@@ -3,15 +3,15 @@ package com.abc1236.ms.enumeration.cms;
 /**
  * 广告类型
  */
-public enum  BannerTypeEnum {
+public enum BannerTypeEnum {
 
     INDEX("index"),
     NEWS("news"),
-    CASE ("case"),
+    CASE("case"),
     PRODUCT("product"),
     SOLUTION("solution");
 
-    private String value;
+    private final String value;
 
     BannerTypeEnum(String value) {
         this.value = value;
