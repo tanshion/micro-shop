cc78352885697f40594bd5949609c4e4415e8a81
tanshion <843565561@qq.com>
2025-01-20 08:43:47 +0800

    refactor(mss): 优化栏目删除逻辑
    - 将栏目删除操作从直接使用 mapper 层方法改为使用 Db 服务层方法- 提高了代码的可维护性和一致性

--- micro-shop-service/src/main/java/com/abc1236/ms/service/cms/impl/ChannelServiceImpl.java
+++ micro-shop-service/src/main/java/com/abc1236/ms/service/cms/impl/ChannelServiceImpl.java
@@ -40,7 +40,7 @@ public class ChannelServiceImpl implements ChannelService {
         log.info("删除栏目");
         Channel channel = Db.getById(id, Channel.class);
         MyAssert.notNull(channel, "栏目不存在");
-        return SqlHelper.retBool(channelMapper.deleteById(id));
+        return Db.removeById(channel);
     }
 
     @Override
